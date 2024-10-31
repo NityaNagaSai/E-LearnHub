97cdb03033446ba5cd44415b20afa5919bd8f8e3
Priya Krishnamurthy <pkrish24@ncsu.edu>
2024-10-31 15:31:25 -0400

    Faculty phase 3

--- app/blueprints/faculty/__init__.py
+++ app/blueprints/faculty/__init__.py
@@ -1,5 +1,7 @@
 from flask import Blueprint
 
-faculty_bp = Blueprint('faculty', __name__, template_folder='templates')
+faculty_bp = Blueprint('faculty', __name__, template_folder='templates', url_prefix='/faculty')
 
 from app.blueprints.faculty import routes
+
+
--- app/blueprints/faculty/routes.py
+++ app/blueprints/faculty/routes.py
@@ -1,5 +1,10 @@
 from flask import Blueprint, render_template, redirect, session, url_for, request, flash
+from rich import _console
 from . import faculty_bp
+import logging
+
+# Configure the logging module
+logging.basicConfig(level=logging.DEBUG)
 
 @faculty_bp.route('/home', methods=['GET', 'POST'])
 def faculty_home():
@@ -8,46 +13,45 @@ def faculty_home():
 # Route for 'Go to Active Course'
 @faculty_bp.route('/active_course')
 def go_to_active_course():
+    print(f"User ID") 
+    print("HERE" + (url_for('faculty.view_worklist')))
     if request.method == 'POST':
         course_id = request.form.get('course_id')
         option = request.form.get('option')
+        session['course_id'] = course_id  # Save the selected course_id for later use
         
-        # Save the selected course_id in session for later use
-        session['course_id'] = course_id
         
         if not course_id:
             flash("Please enter a valid Course ID", "error")
-            return redirect(url_for('go_to_active_course'))
+            return redirect(url_for('faculty.go_to_active_course'))
 
         # Redirect based on the selected option
         if option == '1':
-            return redirect(url_for('view_worklist', course_id=course_id))
+            return redirect(url_for('faculty.view_worklist', course_id=course_id))
         elif option == '2':
-            return redirect(url_for('approve_enrollment', course_id=course_id))
+            return redirect(url_for('faculty.approve_enrollment', course_id=course_id))
         elif option == '3':
-            return redirect(url_for('view_students', course_id=course_id))
+            return redirect(url_for('faculty.view_students', course_id=course_id))
         elif option == '4':
-            return redirect(url_for('add_chapter', course_id=course_id))
+            return redirect(url_for('faculty.add_chapter', course_id=course_id))
         elif option == '5':
-            return redirect(url_for('modify_chapters', course_id=course_id))
+            return redirect(url_for('faculty.modify_chapters', course_id=course_id))
         elif option == '6':
-            return redirect(url_for('add_ta', course_id=course_id))
+            return redirect(url_for('faculty.add_ta', course_id=course_id))
         elif option == '7':
-            return redirect(url_for('faculty_home'))
+            return redirect(url_for('faculty.faculty_home'))
         else:
             flash("Invalid option selected.", "error")
             return redirect(url_for('faculty.go_to_active_course'))
         
-    return render_template('active_course.html')  # Replace with your template
+    return render_template('active_course.html')
 
-# Route for 'Go to Evaluation Course'
 @faculty_bp.route('/evaluation_course')
 def go_to_evaluation_course():
     if request.method == 'POST':
         course_id = request.form.get('course_id')
         option = request.form.get('option')
         
-        # Save the selected course_id in session for later use
         session['course_id'] = course_id
         
         if not course_id:
@@ -59,7 +63,7 @@ def go_to_evaluation_course():
         elif option == '2':
             return redirect(url_for('modify_chapters', course_id=course_id))
         elif option == '3':
-            return redirect('faculty/home')
+            return redirect(url_for('faculty.faculty_home'))
         else:
             flash("Invalid option selected.", "error")
             return redirect(url_for('go_to_active_course'))
@@ -83,6 +87,24 @@ def view_courses():
         flash("Invalid option selected.", "error")
         return redirect(url_for('go_to_active_course'))
    return render_template('view_courses.html', courses=assigned_courses)  # Replace with your template
+
+@faculty_bp.route('/view_worklist', methods=['GET', 'POST'])
+def view_worklist(courseID):
+   waitlisted_students = [{"studentID" : "200535454", "name": "Emma"},
+                       {"studentID" : "257303990", "name": "Daniel"} , 
+                       {"studentID" : "257303980", "name": "Rupert"}]
+
+   if request.method == 'POST':
+   #Query Courses from DB and send it
+   # assigned_courses = Course.query.filter_by(faculty_id=faculty_id).all()
+    option = request.form.get('option')
+    
+    if option == '1':
+        return redirect(url_for('faculty.faculty_home'))
+    else:
+        flash("Invalid option selected.", "error")
+        return redirect(url_for('go_to_active_course'))
+   return render_template('view_worklist.html', waitlist=waitlisted_students, courseID = courseID)  # Replace with your template
 
 # Route for 'Change Password'
 @faculty_bp.route('/faculty/change_password', methods=['GET', 'POST'])
